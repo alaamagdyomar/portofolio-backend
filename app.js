40595b68e98dd9e7b0a4aeb395757d1c625e1457
alaamagdyomar <alaa_magdy@outlook.com>
2021-04-09 02:04:59 +0200

    portofolio-back

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const sendGrid = require('@sendGrid/mail');
+
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
+app.get('/api', (req, res, next) => {
+    res.send('API Status: I\'m awesome')
+});
+
+
+app.post('/api/email', (req, res, next) => {
+
+    console.log(req.body);
+
+    sendGrid.setApiKey('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');
+    const msg = {
+        to: 'alaa_magdy@outlook.com',
+        from: req.body.email,
+        subject: 'Contact me',
+        text: req.body.message
+    }
+
+    sendGrid.send(msg)
+        .then(result => {
+
+            res.status(200).json({
+                success: true
+            });
+
+        })
+        .catch(err => {
+
+            console.log('error: ', err);
+            res.status(401).json({
+                success: false
+            });
+
+        });
+});
+
+
+app.listen(3030, '0.0.0.0');
+console.log('server is running');
